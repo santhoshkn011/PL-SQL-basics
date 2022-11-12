0588848b20f5ee116eb6e9239cfb25476346d942
Santhosh_Kumar_Nayak <102398890+santhoshkn011@users.noreply.github.com>
2022-11-13 01:13:57 +0530

    Add files via upload
    Added: reverse Right Triangle

---
+++ Patterns/StarPattern_ReverseRightTriangle.sql
@@ -0,0 +1,16 @@
+SET SERVEROUTPUT ON; 
+declare
+	n number:=5;
+	i number;
+	j number;
+begin
+	for i in reverse 1..n
+	loop
+		for j in 1..i
+		loop
+			dbms_output.put('*');
+		end loop;
+		
+		dbms_output.new_line;
+	end loop;
+end;
