cfb0ff84e09ef2a0764034a728a7f92b7fed1e66
Santhosh_Kumar_Nayak <102398890+santhoshkn011@users.noreply.github.com>
2022-11-13 15:05:37 +0530

    Update NamedBlock.sql
    Modified: Added CRUD Operation Procedures

--- NamedBlock.sql
+++ NamedBlock.sql
@@ -4,6 +4,7 @@ create table students
 insert into students values(1,'Santhosh','Male');
 
 --Named block
+--Insert Data
 create or replace procedure InsertStudent(
 student_number in students.rollNumber%type,
 student_name in students.studentName%type,
@@ -14,5 +15,31 @@ begin
     commit;
 end;
 
---To execute procedure
-EXECUTE InsertStudent(4, 'Namitha', 'Female');
+--To execute insert procedure
+EXECUTE InsertStudent(5, 'Bhanu Prakash', 'Male');
+
+--Update Data
+create or replace procedure UpdateStudent(
+student_number in students.rollNumber%type,
+student_name in students.studentName%type,
+student_gender in students.gender%type)
+is
+begin
+    update students set rollNumber=student_number, studentName=student_name, gender=student_gender where rollNumber=student_number;
+    commit;
+end;
+
+--To execute update procedure
+EXECUTE UpdateStudent(2, 'Swatika', 'Female');
+
+--Delete Data
+create or replace procedure DeleteStudent(
+student_name in students.studentName%type)
+is
+begin
+    delete from students where studentName=student_name;
+    commit;
+end;
+
+--To execute delete procedure
+EXEC DeleteStudent('Santhosh');
