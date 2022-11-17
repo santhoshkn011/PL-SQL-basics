f030909770c016de3daeb79eec715c7c3e570224
Santhosh_Kumar_Nayak <102398890+santhoshkn011@users.noreply.github.com>
2022-11-17 18:41:00 +0530

    Add files via upload
    Added: Cursor example

---
+++ Cursor_Example.sql
@@ -0,0 +1,23 @@
+SET SERVEROUTPUT ON;
+select * from bankaccount;
+
+declare
+    c_name bankaccount.customername%type;
+    acc_number bankaccount.bankaccountnumber%type;
+    cursor bank_cursor is
+    select customername,bankaccountnumber from bankaccount;
+begin
+    open bank_cursor;
+    
+    fetch bank_cursor into c_name, acc_number;
+    dbms_output.put_line('Account holder name is '||c_name||', Account number is '||acc_number);
+    fetch bank_cursor into c_name, acc_number;
+    dbms_output.put_line('Account holder name is '||c_name||', Account number is '||acc_number);
+    fetch bank_cursor into c_name, acc_number;
+    dbms_output.put_line('Account holder name is '||c_name||', Account number is '||acc_number);
+    fetch bank_cursor into c_name, acc_number;
+    dbms_output.put_line('Account holder name is '||c_name||', Account number is '||acc_number);
+    
+    close bank_cursor;
+end;
+    
