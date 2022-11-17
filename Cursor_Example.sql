3558820065e27535a0e3c22606cbdbda6d1c6d4d
Santhosh_Kumar_Nayak <102398890+santhoshkn011@users.noreply.github.com>
2022-11-17 18:43:57 +0530

    Update Cursor_Example.sql
    Modified: Added loop

--- Cursor_Example.sql
+++ Cursor_Example.sql
@@ -21,3 +21,20 @@ begin
     close bank_cursor;
 end;
     
+    
+--using loop
+declare
+    c_name bankaccount.customername%type;
+    acc_number bankaccount.bankaccountnumber%type;
+    cursor bank_cursor is
+    select customername,bankaccountnumber from bankaccount;
+begin
+    open bank_cursor;
+    
+    loop
+        fetch bank_cursor into c_name, acc_number;
+        dbms_output.put_line('Account holder name is '||c_name||', Account number is '||acc_number);
+        Exit when bank_cursor%NOTFOUND; 
+    end loop;
+    close bank_cursor;
+end;
